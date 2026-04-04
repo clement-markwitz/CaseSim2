f6e181aae8aaf2a7c83d6e60854126e9de2bb264
Markwitz Clement <clem.markwitz@gmail.com>
2026-04-04 17:39:15 +0200

    feat; add type for profile

---
+++ constants/user.ts
@@ -0,0 +1,9 @@
+export interface Profile {
+    id: string;
+    username: string;
+    email: string;
+    avatar: string;
+    balance: number;
+    is_premium: boolean;
+    shop_balance: number;
+}
