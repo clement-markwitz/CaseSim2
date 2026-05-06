b9dc63f8c2916f15d300e4b143de225f34898119
Markwitz Clement <clem.markwitz@gmail.com>
2026-05-07 00:59:09 +0200

    fix; change type of profile

--- constants/user.ts
+++ constants/user.ts
@@ -3,8 +3,11 @@ export interface Profile {
     username: string;
     email: string;
     avatar: string;
-    balance: number;
     is_premium: boolean;
+    balance: number | null;
+    balance_event: number | null;
+    active_weekly_leaderboard_id: number | null;
+    active_event_leaderboard_id: number | null;
     shop_balance: number;
     date_daily_reward: string;
     date_weekly_reward: string;
